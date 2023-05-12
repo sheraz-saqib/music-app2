a77cadef0b799eede43ba2b133cc635ee5fdff9a
sheraz-saqib <bhanakop@gmial.com>
2023-05-12 06:34:34 -0700

    update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const songCollection = [
     audio: new Audio("./song/2.mp3"),
   },
   {
-    name: "Kahani yari",
+    name: "Kana yari",
     img: "3",
     audio: new Audio("./song/3.mp3"),
   },
@@ -49,6 +49,7 @@ const songCollection = [
 ];
 const songImg = document.querySelector(".imgArea img"),
 container = document.querySelector(".container"),
+songplayIndex = document.querySelector(".songplayIndex"),
   song_name = document.querySelector(".song_name p "),
   progressBarArea = document.querySelector(".progressBarArea"),
   progreeLine = document.querySelector(".progreeLine"),
@@ -59,21 +60,29 @@ container = document.querySelector(".container"),
   playIcon = document.querySelector(".bx-play"),
   pauseIcon = document.querySelector(".bx-pause"),
   nextIcon = document.querySelector(".bx-skip-next"),
+  volume_control = document.querySelector(".volume_control"),
+  volume_bar = document.querySelector(".volume_bar"),
+  speakerIconDiv = document.querySelector(".speakerIcon"),
+  speakerIcon = document.querySelector(".speakerIcon i"),
   toggle_icon_li = document.querySelector(".toggle-icon"),
   heart_li = document.querySelector(".heart_li"),
+  control = document.querySelector(".control ul"),
   heartIcon = document.querySelector(".bx-heart");
 let songIndex = 0;
 let song = songCollection[songIndex].audio;
-let songNextPlay = false;
+let songNextPlay = true;
 
 function playSong() {
   song = songCollection[songIndex].audio;
   timeUpdate();
   song.play();
   songImg.src = `./covers/${songIndex + 1}.jpg`;
+  songplayIndex.textContent = songIndex+1;
   song_name.textContent = songCollection[songIndex].name;
   toggle_icon_li.classList.add('play_animation')
   container.classList.add('color_change')
+  control.style.backgroundColor = 'rgb(221, 221, 221)'
+  // volume_control.style.backgroundColor = 'white'
 }
 
 function pauseSong() {
@@ -81,6 +90,7 @@ function pauseSong() {
   song.pause();
   toggle_icon_li.classList.remove('play_animation')
   container.classList.remove('color_change')
+  control.style.backgroundColor = 'rgba(233, 233, 233, 0.37)'
 }
 
 playIcon.onclick = () => {
@@ -146,12 +156,12 @@ previousIcon.onclick = () => {
   song = songCollection[songIndex].audio;
   pauseSong();
   songIndex--;
-  if (songIndex <= 0) {
+  if (songIndex <= 1) {
     songIndex = 8;
   }
   playSong();
 };
-repeatIcon.onclick = () =>  repeatIcon.classList.toggle("icon_active");
+// repeatIcon.onclick = () =>  repeatIcon.classList.toggle("icon_active");
 function timeUpdate() {
   song.addEventListener("timeupdate", () => {
     let songProgress = (song.currentTime / song.duration) * 100;
@@ -166,12 +176,19 @@ function timeUpdate() {
     }
   });
 }
+
 function nextPlay(){
-    if (song.currentTime === song.duration) {
+    if ( song.currentTime === song.duration) {
         nextIcon.click();
     }
 }
-
+repeatIcon.addEventListener('click',()=>{
+  repeatIcon.classList.toggle("icon_active")
+  songNextPlay= false;
+  if(songNextPlay == false){
+    song.play()
+  }
+})
 
 progressBarArea.addEventListener("click", (e) => {
   let progreesWidth = progressBarArea.clientWidth;
@@ -182,15 +199,46 @@ progressBarArea.addEventListener("click", (e) => {
   timeUpdate();
   playIcon.click();
 });
-window.localStorage.setItem('heartLike', heartIcon.classList.contains('bxs-heart'))
-
- let heartlike = window.localStorage.getItem('heartLike')
-heartIcon.addEventListener('click',()=>{
-  heartIcon.classList.toggle('bxs-heart')
-  heartIcon.classList.toggle('bx-heart')
-  heartIcon.classList.toggle('like-heart')
-  song_name.innerHTML= heartlike
-});
 
 
+// window.localStorage.setItem('heartLike', heartIcon.classList.contains('bxs-heart'))
+
+//  let heartlike = window.localStorage.getItem('heartLike')
+// heartIcon.addEventListener('click',()=>{
+//   heartIcon.classList.toggle('bxs-heart')
+//   heartIcon.classList.toggle('bx-heart')
+//   heartIcon.classList.toggle('like-heart')
+//   song_name.innerHTML= heartlike
+// });
+
+volume_control.addEventListener('click',(e)=>{
+
+  let volTotalWidth = volume_control.clientWidth;
+  let volClickWidth = e.offsetX;
+ let volFinalPercent = volClickWidth / volTotalWidth * 100
+ let songVol =volFinalPercent/100; 
+ volume_bar.style.width = `${volFinalPercent}%`
+ song.volume = songVol;
+ if(volFinalPercent >= 99.61){
+  speakerIcon.style.color = 'white'
+  volume_bar.style.borderTopRightRadius = '.8rem'
+  volume_bar.style.borderBottomRightRadius = '.8rem'
+
+ }
+
+  else if(volFinalPercent <= 1.5){
+    volume_bar.style.width = `0%`
+speakerIcon.style.color = ' rgb(230, 62, 40)'
+volume_bar.style.borderTopRightRadius = '.8rem'
+  volume_bar.style.borderBottomRightRadius = '.8rem'
+ }
+ else{
+  volume_bar.style.borderTopRightRadius = '0rem'
+  volume_bar.style.borderBottomRightRadius = '0rem'
+  speakerIcon.style.color = 'white'
+ }
+ 
+
+
+})
 
